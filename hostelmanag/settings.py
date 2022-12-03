677218f35318d82991c8e0b7a149ae13d4f114d7
Gyanendra Shukla <srirajshukla@gmail.com>
2022-12-03 22:51:30 +0530

    feat: protect /graphql with auth0

--- hostelmanag/settings.py
+++ hostelmanag/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv, find_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -166,3 +168,15 @@ GRAPHQL_AUTH = {
         'is_student': 'Boolean' ,
     }
 }
+
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+
+# Load Auth0 application settings into memory
+AUTH0_DOMAIN = os.environ.get("AUTH0_DOMAIN")
+AUTH0_CLIENT_ID = os.environ.get("AUTH0_CLIENT_ID")
+AUTH0_CLIENT_SECRET = os.environ.get("AUTH0_CLIENT_SECRET")
+
+TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
--- hostelmanag/urls.py
+++ hostelmanag/urls.py
@@ -14,13 +14,53 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
 
 from graphene_django.views import GraphQLView
 
+#oauth
+from authlib.integrations.django_client import OAuth
+from django.conf import settings
+from django.urls import reverse
+from django.shortcuts import redirect
+
+
+oauth = OAuth()
+
+oauth.register(
+    "auth0",
+    client_id=settings.AUTH0_CLIENT_ID,
+    client_secret=settings.AUTH0_CLIENT_SECRET,
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f"https://{settings.AUTH0_DOMAIN}/.well-known/openid-configuration",
+)
+
+
+def protected_graphql_view(request):
+    return oauth.auth0.authorize_redirect(
+        request, request.build_absolute_uri(reverse("callback"))
+    )
+
+
+def callback(request):
+    token = oauth.auth0.authorize_access_token(request)
+    request.session["user"] = token
+    return redirect(request.build_absolute_uri(reverse("graphql")))
+
+#
+
+def verify(request):
+    userId = request.GET.get('userId')
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path("graphql_entry/", protected_graphql_view),
+    path("callback/", callback, name="callback"),
+    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True)), name="graphql"),
+    path('verify/', verify, name="verify"),
 ]
