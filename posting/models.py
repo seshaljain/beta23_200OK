b067094ac6527c57599496828cd232fe6460f7b6
Gyanendra Shukla <srirajshukla@gmail.com>
2022-12-03 14:09:06 +0530

    feat: creat post model

--- posting/models.py
+++ posting/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey('user.AbstractHostelUser', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    tags = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
